dad43b3e80d098f484a27dba08bd4707e815ce6a
Ondairos <ondairos@gmail.com>
2023-01-16 14:39:15 +0200

    changed for with vanilla one for counting likes

--- utils/totalLikes.js
+++ utils/totalLikes.js
@@ -1,8 +1,8 @@
 // Define a new totalLikes function that receives a list of blog posts as a parameter. The function returns the total sum of likes in all of the blog posts.
 const checkTotalLikes = (blogPosts) => {
     let totalLikesCount = 0
-    for (let element in blogPosts) {
-        totalLikesCount = totalLikesCount + element.likes
+    for (let i = 0; i < blogPosts.length; i++) {
+        totalLikesCount = totalLikesCount + blogPosts[i].likes
     }
     return totalLikesCount
 }
