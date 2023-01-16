ee66525c8a4d8bacffe7416201fcd95aebe79ae3
Ondairos <ondairos@gmail.com>
2023-01-16 14:30:53 +0200

    added total likes testing

--- utils/totalLikes.js
+++ utils/totalLikes.js
@@ -1,6 +1,10 @@
 // Define a new totalLikes function that receives a list of blog posts as a parameter. The function returns the total sum of likes in all of the blog posts.
-const checkTotalLikes = (blogPost) => {
-    return blogPost.likes
+const checkTotalLikes = (blogPosts) => {
+    let totalLikesCount = 0
+    for (let element in blogPosts) {
+        totalLikesCount = totalLikesCount + element.likes
+    }
+    return totalLikesCount
 }
 
 module.exports = {
