242dd5fa7f5363baf7fd72d00c855f91249fd34e
Ondairos <ondairos@gmail.com>
2023-01-17 15:04:14 +0200

    changes to blog test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const config = require('../utils/config')
 
 // import the Express application from the app.js module and wrap it with the supertest function into a so-called superagent object
 const api = supertest(app)
 // supertest port: if the server is not already listening for connections then it is bound to an ephemeral port for you so there is no need to keep track of ports.
 
+beforeAll(async () => {
+    await mongoose.connect(config.mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+})
 
 test('blog posts are returned as json', async () => {
     await api
@@ -14,19 +18,22 @@ test('blog posts are returned as json', async () => {
         .expect('Content-Type', /application\/json/)
 }, 100000)
 
-afterAll(() => {
-    mongoose.connection.close()
-})
 
-// Both tests store the response of the request to the response variable, and unlike the previous test that used the methods provided by supertest for verifying the status code and headers
+
 test('there are two blog posts', async () => {
+    console.log('Making GET request to /api/blogs')
     const response = await api.get('/api/blogs')
-
-    expect(response.body).toHaveLength(2)
+    console.log('Response received:', response.body)
+    expect(response.body).toHaveLength(3)
 })
 
 test('the first blog post is about', async () => {
+    console.log('Making GET request to /api/blogs')
     const response = await api.get('/api/blogs')
+    console.log('Response received:', response.body[0])
+    expect(response.body[0].title).toBe('The earth spirit')
+})
 
-    expect(response.body[0].title).toBe('The athletic spirit')
+afterAll(() => {
+    mongoose.connection.close()
 })
