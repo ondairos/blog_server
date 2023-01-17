ddec4d79df97c489c23690ae98825d3a78178d71
Ondairos <ondairos@gmail.com>
2023-01-17 16:02:05 +0200

    refactored blog tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,14 +1,48 @@
+/* eslint-disable no-trailing-spaces */
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const config = require('../utils/config')
+const blog = require('../models/blog')
+// const config = require('../utils/config')
 
 // import the Express application from the app.js module and wrap it with the supertest function into a so-called superagent object
 const api = supertest(app)
 // supertest port: if the server is not already listening for connections then it is bound to an ephemeral port for you so there is no need to keep track of ports.
 
-beforeAll(async () => {
-    await mongoose.connect(config.mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+// beforeAll(async () => {
+//     await mongoose.connect(config.mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+// })
+
+const initialBlogPosts = [
+    {
+        'title': 'TEST: The One spirit',
+        'author': '231Z',
+        'url': 'first_blog_post',
+        'likes': 22
+    },
+    {
+        'title': 'The two spirit',
+        'author': '231Z',
+        'url': 'second_blog_post',
+        'likes': 11
+    },
+    {
+        'title': 'The three spirit',
+        'author': '231Z',
+        'url': 'third_blog_post',
+        'likes': 3
+    },
+]
+
+// clear database and add three test blog posts
+beforeEach(async () => {
+    await blog.deleteMany({})
+    let blogObject = new blog(initialBlogPosts[0])
+    await blogObject.save()
+    blogObject = new blog(initialBlogPosts[1])
+    await blogObject.save()
+    blogObject = new blog(initialBlogPosts[2])
+    await blogObject.save()
 })
 
 test('blog posts are returned as json', async () => {
@@ -23,15 +57,16 @@ test('blog posts are returned as json', async () => {
 test('there are two blog posts', async () => {
     console.log('Making GET request to /api/blogs')
     const response = await api.get('/api/blogs')
-    console.log('Response received:', response.body)
-    expect(response.body).toHaveLength(3)
+    
+    expect(response.body).toHaveLength(initialBlogPosts.length)
 })
 
-test('the first blog post is about', async () => {
+test('a specific blog post is within the returned blog posts', async () => {
     console.log('Making GET request to /api/blogs')
     const response = await api.get('/api/blogs')
-    console.log('Response received:', response.body[0])
-    expect(response.body[0].title).toBe('The earth spirit')
+
+    const blogTitles = response.body.map(element => element.title)
+    expect(blogTitles).toContain('The two spirit')
 })
 
 afterAll(() => {
