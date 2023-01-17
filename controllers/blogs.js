db2ee75fb83ea68250aee5bf6d143eb5d6edf7f9
Ondairos <ondairos@gmail.com>
2023-01-17 18:00:28 +0200

    refactored POST route with async await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,7 +2,7 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 // const mongoose = require('mongoose')
 
-// home route
+
 // blogsRouter.get('/', (request, response) => {
 //     Blog
 //         .find({})
@@ -11,19 +11,34 @@ const Blog = require('../models/blog')
 //         })
 // })
 
+// home route
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
+// blogsRouter.post('/', (request, response, next) => {
+//     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        }).catch(error => next(error))
+//     blog
+//         .save()
+//         .then(result => {
+//             response.status(201).json(result)
+//         }).catch(error => next(error))
+// })
+
+blogsRouter.post('/', async (request, response, next) => {
+    const body = request.body
+
+    const blogPost = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    })
+
+    const savedBlogPost = await blogPost.save()
+    response.status(201).json(savedBlogPost)
 })
 
 module.exports = blogsRouter
